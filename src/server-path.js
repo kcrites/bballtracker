d324fbd3d20e5648f973cf735fae531aca2e52e5
Ken Crites <kenrcrites@gmail.com>
2020-10-03 12:39:30 +0200

    Added  correct server URL

--- src/server-path.js
+++ src/server-path.js
@@ -1,2 +1,3 @@
-export const serverURL = 'http://localhost:3005/';
+//export const serverURL = 'http://localhost:3005/';
+export const serverURL = 'https://cryptic-wildwood-13483.herokuapp.com/';
 //export const serverURL = 'https://bballserver-ams.herokuapp.com/';
